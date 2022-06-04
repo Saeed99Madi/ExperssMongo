c5082ba71ba60754cf87595cd8b3677934408ee9
Saeed99Madi <saed.zooro@gmail.com>
2022-06-04 17:07:40 +0300

    install mongoose DB

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 //app.js
+const express = require('express');
+const app = express();
+
+
+//Middlewares 
+
+
+//Routes
+app.get('/', (req, res) => {
+    res.send('we are on home');
+});
+
+//Serve
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log("listening on PORT", PORT);
+});
